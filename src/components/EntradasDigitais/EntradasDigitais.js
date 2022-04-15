1bbb41d43f54f69b13be13d57b01f145c49f1ada
PabloErlis <pabloerlis@gmail.com>
2022-04-15 08:51:08 -0300

    Coloquei um relógio no componente EntradasDigitais só pra colocar em prática o aprendizado de ciclo de vida.

--- src/components/EntradasDigitais/EntradasDigitais.js
+++ src/components/EntradasDigitais/EntradasDigitais.js
@@ -1,8 +1,38 @@
 import React from "react";
 
 class EntradasDigitais extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
   render() {
-    return <div>Entradas Digitais funcionando!</div>;
+    return (
+      <div>
+        <h1>Entradas Digitais funcionando</h1>
+
+        <h2>Hora {this.state.date.toLocaleTimeString()}</h2>
+      </div>
+    );
   }
 }
 
