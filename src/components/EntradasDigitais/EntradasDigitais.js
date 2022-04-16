a70884363452d1202b980d55f45fc2b513d422fd
PabloErlis <pabloerlis@gmail.com>
2022-04-16 08:38:55 -0300

    Construindo as entradas digitais

--- src/components/EntradasDigitais/EntradasDigitais.js
+++ src/components/EntradasDigitais/EntradasDigitais.js
@@ -1,38 +1,45 @@
 import React from "react";
+import "./EntradasDigitais.css";
 
+let entradas_8 = [1, 1, 1, 1, 1, 1, 1, 1];
+let entradas_64 = [
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+];
 class EntradasDigitais extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = { date: new Date() };
+    this.state = {};
   }
 
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
+  componentDidMount() {}
 
-      1000
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date(),
-    });
-  }
+  componentWillUnmount() {}
 
   render() {
     return (
       <div>
-        <h1>Entradas Digitais funcionando</h1>
-
-        <h2>Hora {this.state.date.toLocaleTimeString()}</h2>
+        <h3>Entradas Digitais(CLP)</h3>
+        {this.construirBotoes(entradas_8)}
       </div>
     );
+  }
+
+  construirBotoes(entradas) {
+    let botoes = [];
+    for (var i = 0; i < entradas.length; i++) {
+      botoes.push(
+        <button
+          className={
+            entradas[i] === 1 ? "btn btn-success" : "btn btn-seccondary"
+          }
+        >
+          {i}
+        </button>
+      );
+    }
+    return botoes;
   }
 }
 
